3e9a674a268415c7e353357ac598464715e7fe88
Bastiu37836 <105975580+Bastiu37836@users.noreply.github.com>
2025-12-04 10:58:38 +0100

    setup basic files

---
+++ script.js
@@ -0,0 +1,14 @@
+/* --------------------------------------------------- */
+/* script.js */
+/* --------------------------------------------------- */
+
+
+// Einfache JS-Interaktion
+const btn = document.getElementById("actionBtn");
+
+
+if (btn) {
+btn.addEventListener("click", () => {
+alert("Button funktioniert! Hier kommt später echte Funktionalität.");
+});
+}
